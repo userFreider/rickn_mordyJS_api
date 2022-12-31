e1bd4ac0c96311c2eb32902e081872a1130b2a57
freyder montano <freymanuser@gmail.com>
2022-12-31 14:38:39 -0300

    mostrar informacion personaje con fragment

--- getDataApi/getDataPromise.js
+++ getDataApi/getDataPromise.js
@@ -1,9 +1,37 @@
 const api = 'https://rickandmortyapi.com/api/character';
 
-const getData = (apiUrl)=> {
-    fetch(apiUrl)
+const getData = (apiUrl) => {
+  fetch(apiUrl)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(json => displayData(json))
+
+}
+
+//imprimir datos
+const displayData = (data) => {
+
+
+  data.results.forEach(persj => {
+
+    const article = document.createRange().createContextualFragment(`
+    
+        <div class="card m-2 p-2 shadow" style="width: 18rem;">
+          <img src="${persj.image}" class="card-img-top " alt="personajes">
+          <div class="card-body">
+            <h5 class="card-title"><span class="fw-bold">Name</span>:${persj.name}</h5>
+            <p class="card-text"><span class="fw-bold">Gender</span> :${persj.gender}</p>
+            <p class="card-text"><span class="fw-bold">Statust</span> :${persj.status}</p>
+            
+          </div>
+        </div>
+          `);
+
+    const div = document.querySelector('#informCaracters');
+    div.append(article)
+
+  });
+
+
 
 }
 
